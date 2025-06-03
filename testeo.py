ee5d4f1cfb8afd0cf5e9b6ce4171715cb8b983d2
Alejandro García Pérez <alegp97@gmail.com>
2025-06-03 17:18:08 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -17,3 +17,10 @@ object ScenariosDFConstants {
   val Timeline = "scenariosDF_by_unit_entity_exercise.timeline"
   val Year = "scenariosDF_by_unit_entity_exercise.year"
 }
+
+
+private static final String SCENARIOS_1 = "scenarios_1";
+private static final String SCENARIOS_2 = "scenarios_2";
+private static final String MODIFICATIONS_TAXONOMY = "modifications_taxonomy";
+private static final String HISTORICAL_DATA = "historical_data";
+
