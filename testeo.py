9ec98d8c10bcff753e32f5ce2ceea7409559593e
Alejandro García Pérez <alegp97@gmail.com>
2025-06-10 17:04:12 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -5,20 +5,9 @@ object DropRecordDriver extends HdfsFunctions {
 
   def main(args: Array[String]): Unit = {
     log.info(s"[SAST] - Starting drop elements")
-    val parsedArgs = parseArgsOrExit(args)
-    val spark = createSparkSession()
+    val parsedArgs = ArgsParser.parse(args, Args()).fold(ifEmpty = sys.exit(1))
 
-    SparkContextUtil.withHiveContext(DRIVER_APP_NAME) { implicit spark =>
-      configureSparkAndHadoop()
-      processFile(parsedArgs.file)
-    }
-  }
-
-  private def parseArgsOrExit(args: Array[String]): Args =
-    ArgsParser.parse(args, Args()).fold(ifEmpty = sys.exit(1))
-
-  private def createSparkSession(): SparkSession =
-    SparkSession.builder
+    val spark = SparkSession.builder
       .appName(DRIVER_APP_NAME)
       .config("hive.exec.dynamic.partition.mode", "nonstrict")
       .config("hive.metastore.try.direct.sql", "true")
@@ -26,7 +15,27 @@ object DropRecordDriver extends HdfsFunctions {
       .enableHiveSupport()
       .getOrCreate()
 
-  private def configureSparkAndHadoop()(implicit spark: SparkSession): Unit = {
+    SparkContextUtil.withHiveContext(DRIVER_APP_NAME) { implicit spark =>
+      configureSparkContext(spark)
+
+      val fileName = parsedArgs.file
+      val filePath = new Path(fileName)
+      val fs = HDFSHandler.getFileSystem(fileName)
+
+      if (!fs.exists(filePath)) {
+        log.error(s"[SAST] - File $fileName not found")
+        throw new EresearchFileNotFoundException(s"The file : $fileName not exists")
+      }
+
+      val conf = Iterator.continually(HDFSHandler.getStream(fileName).readLine())
+        .takeWhile(_ != null).mkString
+      val entity = parse(conf, true).extract[CleanEntity]
+      processEntityInputs(entity, fileName, fs)
+      fs.rename(new Path(fileName), new Path(fileName + "." + System.currentTimeMillis()))
+    }
+  }
+
+  private def configureSparkContext(spark: SparkSession): Unit = {
     val sc = spark.sqlContext
     sc.setConf("spark.sql.tungsten.enabled", "false")
     sc.setConf("spark.sql.hive.convertMetastoreParquet", "false")
@@ -37,44 +46,33 @@ object DropRecordDriver extends HdfsFunctions {
     sc.sql("set parquet.compression=GZIP")
   }
 
-  private def processFile(fileName: String)(implicit spark: SparkSession): Unit = {
-    val fs = HDFSHandler.getFileSystem(fileName)
-    val filePath = new Path(fileName)
+  private def processEntityInputs(entity: CleanEntity, fileName: String, fs: FileSystem)
+                                 (implicit spark: SparkSession): Unit = {
+    entity.inputs.foreach { input =>
+      val table = s"${input.database}.${input.table}"
 
-    if (!fs.exists(filePath)) {
-      log.error(s"[SAST] - File $fileName not found")
-      throw new EresearchFileNotFoundException(s"The file : $fileName not exists")
-    }
+      if (input.elements_are_partitions.getOrElse(false)) {
+        input.elements.foreach { element =>
+          val path = HiveUtil.getLocationTable(input.database, input.table) + "/" +
+                     element.toRemove.mkString("/").replace("\"", "")
+          val pathObj = new Path(path)
+          if (fs.exists(pathObj)) fs.delete(pathObj, true)
+          spark.sqlContext.sql(s"ALTER TABLE $table DROP IF EXISTS PARTITION (${element.toRemove.mkString(",")})")
+        }
+      } else {
+        val filter = input.partition_filter.get.mkString(" and ")
+        val partitionFilter = spark.sqlContext.table(input.partition_table.get).where(filter)
 
-    val conf = Iterator.continually(HDFSHandler.getStream(fileName).readLine())
-      .takeWhile(_ != null).mkString
-    val entity = parse(conf, true).extract[CleanEntity]
-    entity.inputs.foreach(input => processInput(input, fileName, fs))
-    fs.rename(new Path(fileName), new Path(fileName + "." + System.currentTimeMillis()))
-  }
-
-  private def processInput(input: CleanInput, fileName: String, fs: FileSystem)
-                          (implicit spark: SparkSession): Unit = {
-    val table = s"${input.database}.${input.table}"
-    if (input.elements_are_partitions.getOrElse(false)) {
-      input.elements.foreach { element =>
-        val path = HiveUtil.getLocationTable(input.database, input.table) + "/" +
-                   element.toRemove.mkString("/").replace("\"", "")
-        val pathObj = new Path(path)
-        if (fs.exists(pathObj)) fs.delete(pathObj, true)
-        spark.sqlContext.sql(s"ALTER TABLE $table DROP IF EXISTS PARTITION (${element.toRemove.mkString(",")})")
-      }
-    } else {
-      val filter = input.partition_filter.get.mkString(" and ")
-      val partitionFilter = spark.sqlContext.table(input.partition_table.get).where(filter)
-      input.elements.foreach { element =>
-        val filtro = element.toRemove.mkString(" and ")
-        val target = spark.sqlContext.table(table).where(filtro)
-        val diff = partitionFilter.except(target)
-        val tmpPath = new Path(fileName).getParent + "/ruta_temp" + System.currentTimeMillis()
-        diff.write.format("parquet").save(tmpPath)
-        spark.sqlContext.read.parquet(tmpPath).write.mode("overwrite").format("parquet").insertInto(table)
-        fs.delete(new Path(tmpPath), true)
+        input.elements.foreach { element =>
+          val filtro = element.toRemove.mkString(" and ")
+          val target = spark.sqlContext.table(table).where(filtro)
+          val diff = partitionFilter.except(target)
+          val tmpPath = new Path(fileName).getParent + "/ruta_temp" + System.currentTimeMillis()
+          diff.write.format("parquet").save(tmpPath)
+          spark.sqlContext.read.parquet(tmpPath)
+            .write.mode("overwrite").format("parquet").insertInto(table)
+          fs.delete(new Path(tmpPath), true)
+        }
       }
     }
   }
