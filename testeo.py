6fcf668fe088f3ec8b48bcc6a1ea0616cc22ae9e
Alejandro García Pérez <alegp97@gmail.com>
2025-06-12 16:31:22 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,4 +1,5 @@
-// Mocks
+
+    // Mocks
     val sourceDF = mock[DataFrame]
     val contextDF = mock[DataFrame]
     val joinedDF = mock[DataFrame]
@@ -19,11 +20,31 @@
     when(contextDF.drop("supra_source")).thenReturn(contextDF)
 
     when(sourceDF.col(eqTo("workspace"))).thenReturn(col("workspace"))
-    when(sourceDF.col(any[String])).thenReturn(col("dummy"))))
+    when(sourceDF.col(any[String])).thenReturn(col("dummy"))
     when(contextDF.col(any[String])).thenAnswer(inv => col(inv.getArgument(0)))
 
     when(sourceDF.join(any[DataFrame], any[Column], any[String])).thenReturn(joinedDF)
     when(joinedDF.drop(any[Column])).thenReturn(joinedDF)
-    when(joinedDF.select(any[Array[Column]])).thenReturn(selectedDF)
+    when(joinedDF.select(any[Array[Column]](): _*)).thenReturn(selectedDF)
     when(selectedDF.withColumnRenamed(any[String], any[String])).thenReturn(renamedDF)
     when(renamedDF.dropDuplicates()).thenReturn(renamedDF)
+
+    // Partition key presente
+    when(renamedDF.select(any[Array[Column]](): _*)).thenReturn(withPKDF)
+    val partitionRow = mock[Row]
+    when(partitionRow.getString(0)).thenReturn("value")
+    when(withPKDF.collect()).thenReturn(Array(partitionRow))
+
+    // Transformaciones
+    when(renamedDF.select(any[Array[Column]](): _*)).thenReturn(renamedDF)
+    when(renamedDF.withColumn(any[String], any[Column])).thenReturn(renamedDF)
+    when(renamedDF.drop(eqTo("data_date_part"))).thenReturn(renamedDF)
+    when(renamedDF.columns).thenReturn(Array("num_col", "weight_inout"))
+
+    when(renamedDF.withColumn(eqTo("row_count"), any[Column])).thenReturn(withRowCountDF)
+    when(withRowCountDF.withColumn(any[String], any[Column])).thenReturn(castedDF)
+    when(castedDF.withColumn(any[String], any[Column])).thenReturn(finalDF)
+    when(finalDF.columns).thenReturn(Array("x"))
+
+    when(renamedDF.count()).thenReturn(10L)
+    when(finalDF.count()).thenReturn(10L)
