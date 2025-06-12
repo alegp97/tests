32173bea18ade245086c250793292526f1b6dd24
Alejandro García Pérez <alegp97@gmail.com>
2025-06-12 18:05:36 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,13 +1,12 @@
-    // ---------- bloque final transformaciones ----------
-    val df_partition = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
+    // partition_key detectado
+    // ---- row_count + drop(data_date_part) chain ----
+    when(renamedDF.withColumn(eqTo("row_count"), any[Column])).thenReturn(withRowCountDF)
+    when(withRowCountDF.drop(eqTo("data_date_part"))).thenReturn(df_partition)
+    when(withRowCountDF.columns).thenReturn(Array("num_col","weight_inout"))
+    when(withRowCountDF.withColumn(eqTo("num_col"), any[Column])).thenReturn(df_partition)
+    when(withRowCountDF.withColumn(eqTo("weight_inout"), any[Column])).thenReturn(df_partition)
 
-    when(renamedDF.drop(eqTo("data_date_part"))).thenReturn(df_partition)
-    when(df_partition.columns).thenReturn(Array("num_col","weight_inout"))
-    when(df_partition.withColumn(eqTo("num_col"), any[Column])).thenReturn(df_partition)
-    when(df_partition.withColumn(eqTo("weight_inout"), any[Column])).thenReturn(df_partition)
-    when(df_partition.withColumn(eqTo("row_count"), any[Column])).thenReturn(df_partition)
-    // encadenado extra para select / withColumnRenamed / dropDuplicates
-    when(df_partition.select(any[Array[Column]](): _*)).thenReturn(df_partition)
-    when(df_partition.withColumnRenamed(any[String], any[String])).thenReturn(df_partition)
-    when(df_partition.dropDuplicates()).thenReturn(df_partition)
-    when(df_partition.count()).thenReturn(10L)
+    when(renamedDF.select(any[Array[Column]](): _*)).thenReturn(withPKDF)
+    val pkRow = mock[Row]
+    when(pkRow.getString(0)).thenReturn("value")
+    when(withPKDF.collect()).thenReturn(Array(pkRow))
