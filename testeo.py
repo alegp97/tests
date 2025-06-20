b61bdfb38fd554f1a7a7bfebc4d1dd6d43d414a8
Alejandro García Pérez <alegp97@gmail.com>
2025-06-20 13:19:26 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,2 +1,10 @@
-java -jar ${HOME}/.m2/repository/org/jacoco/org.jacoco.cli/0.8.11/org.jacoco.cli-0.8.11-nodeps.jar \
+# 1. Inspecciona el contenido
+java -jar ~/.m2/repository/org/jacoco/org.jacoco.cli/0.8.11/*.jar \
      execinfo target/jacoco.exec
+
+# 2. Genera el informe en consola, modo verbose
+java -jar ~/.m2/repository/org/jacoco/org.jacoco.cli/0.8.11/*.jar \
+     report target/jacoco.exec \
+     --classfiles target/classes \
+     --sourcefiles src/main/java \
+     --html target/manual-report -v
