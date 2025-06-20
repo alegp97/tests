1e8230ee2a95831c9f1127f144c169f12893e6a4
Alejandro García Pérez <alegp97@gmail.com>
2025-06-20 14:38:21 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -13,3 +13,27 @@ java -jar ~/.m2/repository/org/jacoco/org.jacoco.cli/0.8.11/*.jar \
 
 java -cp "C:\Users\x068801\.m2\repository\org\jacoco\org.jacoco.cli\0.8.11\org.jacoco.cli-0.8.11.jar" org.jacoco.cli.internal.Main execinfo target/jacoco.exec
 
+<execution>
+  <id>jacoco-report</id>
+  <phase>verify</phase>
+  <goals><goal>report</goal></goals>
+
+  <configuration>
+    <!-- 1. carpeta de salida Java normal -->
+    <classFiles>
+      <param>${project.build.outputDirectory}</param>
+
+      <!-- 2. carpetas Scala (todas las versiones) -->
+      <param>${project.build.directory}/scala-*/classes</param>
+
+      <!-- 3. el/los JARs generados por shade -->
+      <param>${project.build.directory}/${project.build.finalName}-*.jar</param>
+    </classFiles>
+
+    <!-- Si quieres que enlace al código Scala -->
+    <sourceFiles>
+      <param>src/main/scala</param>
+      <param>src/main/java</param>
+    </sourceFiles>
+  </configuration>
+</execution>
