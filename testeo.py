cb436c579e57c49de32154ab6bb461eac4837a1b
Alejandro García Pérez <alegp97@gmail.com>
2025-06-26 13:03:29 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,10 +1,15 @@
-azureEmailSenderMock.when(() =>
-  AzureEmailSender.sendEmail(
-    any[String],
-    any[String],
-    any[Array[String]],
-    any[Array[String]],
-    any[Array[String]],
-    any[Array[String]]
+val azureStatic = mockStatic(classOf[AzureEmailSender])
+
+// 2️⃣  stub: TODOS los argumentos son matchers, firma idéntica
+azureStatic
+  .when(() =>
+    AzureEmailSender.sendEmail(
+      anyString(),                       // subject
+      anyString(),                       // body
+      any(classOf[Array[String]]),       // to
+      any(classOf[Array[String]]),       // cc
+      any(classOf[Array[String]]),       // bcc
+      any(classOf[Array[String]])        // attachments
+    )
   )
-).thenReturn(())
+  .thenAnswer(_ => ())
