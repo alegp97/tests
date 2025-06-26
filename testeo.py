12a348ec5e74beecb130933f5ebad5a1e1f84667
Alejandro García Pérez <alegp97@gmail.com>
2025-06-26 12:35:45 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,25 +1,10 @@
-test("Debe escribir datos correctamente en el destino") {
-  val sparkMock = mock[SparkSession]
-  val sqlContextMock = mock[SQLContext]
-  val mockDataFrame = mock[DataFrame]
-
-  when(sparkMock.sqlContext).thenReturn(sqlContextMock)
-  when(sqlContextMock.table(anyString())).thenReturn(mockDataFrame)
-
-  // Ejecuta el bloque con los mocks activos
-  HiveUtilWrapper.withMocks(
-    dbMock = {
-      case "external_db" => true
-      case _             => false
-    },
-    tableMock = {
-      case ("source_db", "historical_data_total") => true
-      case ("external_db", "pais_moneda")         => true
-      case _                                       => false
-    }
-  ) {
-    BoardHistoricalDataJob.run("source_db", "target_db", "external_db", "2023-01-01")(sparkMock)
-  }
-
-  assert(true)
-}
+azureEmailSenderMock.when(() =>
+  AzureEmailSender.sendEmail(
+    any[String],
+    any[String],
+    any[Array[String]],
+    any[Array[String]],
+    any[Array[String]],
+    any[Array[String]]
+  )
+).thenReturn(())
