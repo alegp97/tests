b926ae17ffc064f5a203b988f79c341fae5ca469
Alejandro García Pérez <alegp97@gmail.com>
2025-07-02 13:20:11 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,38 +1,4 @@
-  test("columnNotInColumn devuelve las columnas de big que no están en small") {
-
-    // 1. Creamos los mocks
-    val sessionMock = mock(classOf[SparkSession], Answers.RETURNS_DEEP_STUBS)
-    val builderMock = mock(classOf[SparkSession.Builder], Answers.RETURNS_DEEP_STUBS)
-
-    // 2. Encadenamos la API fluida
-    when(builderMock.appName(anyString())).thenReturn(builderMock)
-    when(builderMock.config(anyString(), anyString())).thenReturn(builderMock)
-    when(builderMock.enableHiveSupport()).thenReturn(builderMock)
-    when(builderMock.getOrCreate()).thenReturn(sessionMock)
-
-    // 3. Mockeamos SparkSession.builder()
-    val staticSpark: MockedStatic[SparkSession] = mockStatic(classOf[SparkSession])
-    staticSpark.when(() => SparkSession.builder()).thenReturn(builderMock)
-
-    try {
-      // 4. Mocks de Columnas individuales (cada uno único)
-      val colA = mock(classOf[Column])
-      val colB = mock(classOf[Column])
-      val colC = mock(classOf[Column])
-      val colD = mock(classOf[Column])
-
-      when(colA.toString).thenReturn("a")
-      when(colB.toString).thenReturn("b")
-      when(colC.toString).thenReturn("c")
-      when(colD.toString).thenReturn("d")
-
-      val big = List(colA, colB, colC)
-      val small = List(colB, colD)
-
-      val result = BoardDataUtil.columnNotInColumn(big, small)
-
-      assert(result.map(_.toString) == List("a", "c"))
-    } finally {
-      staticSpark.close() // Muy importante
-    }
-  }
+// Mock que evita llamadas a Hadoop
+  when(spark.sparkContext.hadoopConfiguration).thenReturn(new org.apache.hadoop.conf.Configuration {
+    override def get(key: String): String = null
+  })
