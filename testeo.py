8f961d2a66d91341792fb48bcc78edbfb6a3c540
Alejandro García Pérez <alegp97@gmail.com>
2025-07-10 14:09:47 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,52 +1,7 @@
-test("createTmpTable should handle extraFilter not null and StringType with empty where") {
-  val sqlContext = mock[SQLContext](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
-  val sourceDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
-  val filteredDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
-  val tmpDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
-  val distinctDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
+val mockRow = mock[Row](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
 
-  val extraFilter = mock[Column]
-  val column = col("dummy_column")
-  val partList = List("part_col")
+when(mockRow.get(0)).thenReturn("dummy_value")
+when(mockRow.apply(0)).thenReturn("dummy_value")
+when(mockRow.toString()).thenReturn("dummy_value")
 
-  val mockRow = mock[Row]
-  when(mockRow.get(0)).thenAnswer(_ => "dummy_value")
-  val collected = Array(mockRow)
-
-  // Schema mock
-  val structType = StructType(Seq(StructField("part_col", StringType)))
-  when(filteredDF.schema("part_col").dataType.getClass.getSimpleName).thenReturn("StringType$")
-  when(filteredDF.schema).thenReturn(structType)
-
-  // Path mock
-  val path = "/tmp/fake_path"
-  val finalPath = new Path(path + "/tmp_windowsFunction_" + Calendar.getInstance().getTimeInMillis)
-
-  // Source filter returns filteredDF
-  when(sourceDF.where(extraFilter)).thenReturn(sourceDF)
-  when(sourceDF.where(extraFilter).select(any[Seq[Column]])).thenReturn(filteredDF)
-
-  // Select logic
-  when(filteredDF.select(any[Seq[Column]])).thenReturn(filteredDF)
-  when(filteredDF.persist(StorageLevel.MEMORY_AND_DISK)).thenReturn(filteredDF)
-  when(filteredDF.select(col("`part_col`")).distinct().collect()).thenReturn(collected)
-  when(filteredDF.select(col("`part_col`"))).thenReturn(filteredDF)
-  when(filteredDF.unpersist()).thenReturn(filteredDF)
-
-  // tmpDF and distinct write
-  when(sqlContext.sql(any[String])).thenReturn(tmpDF)
-  when(tmpDF.select(any[Seq[Column]])).thenReturn(tmpDF)
-  when(tmpDF.distinct()).thenReturn(distinctDF)
-
-  val resultPath = BoardDataUtil.createTmpTable(
-    sourceDF,
-    "target_table",
-    partList,
-    extraFilter,
-    column,
-    sqlContext,
-    path
-  )
-
-  assert(resultPath.toString.contains("tmp_windowsFunction"))
-}
+val collected = Array(mockRow)
