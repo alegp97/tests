895814b2557e72d1f9c6d7ebd140caf0f0507df2
Alejandro García Pérez <alegp97@gmail.com>
2025-07-10 13:06:10 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,76 +1,49 @@
-test("getSourceFilter should apply filter when entities are not empty and partitionType is valid") {
-  val sqlContext = mock[SQLContext]
-  val dfMock = mock[DataFrame]
-  val filteredDfMock = mock[DataFrame]
-  val colMock = mock[Column]
+test("createTmpTable should generate and save filtered tmp table") {
+    val sqlContext = mock[SQLContext](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
 
-  val partitionType = "data_date_part"
-  val source = "my_table"
-  val query = List(mock[Column])
+    val df = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
+    val filteredDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
+    val tmpDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
+    val distinctDF = mock[DataFrame](withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
 
-  val entity = mock[IngestEntity]
-  when(entity.getDataDatePart).thenReturn("2023-01-01")
-  when(entity.getDataTimestampPart()).thenReturn("2023-01-01 00:00:00")
+    val column = col("dummy_column")
+    val partList = List("part_col")
 
-  when(sqlContext.table(source)).thenReturn(dfMock)
+    val mockRow = mock[Row]
+    when(mockRow.get(0)).thenReturn("value")
+    val collected = Array(mockRow)
 
-  when(dfMock.where(any[Column])).thenReturn(filteredDfMock)
+    // simulate schema
+    val structType = StructType(Seq(StructField("part_col", StringType)))
+    when(df.schema).thenReturn(structType)
+    when(df.schema("part_col").dataType).thenReturn(StringType)
 
-  val result = BoardDataUtil.getSourceFilter(partitionType, source, query, List(entity), sqlContext)
+    // simulate path
+    val path = "/tmp/fake_path"
+    val finalPath = new Path(path + "/tmp_windowsFunction_" + Calendar.getInstance().getTimeInMillis)
 
-  assert(result eq filteredDfMock)
+    // simulate logic
+    when(df.select(any[Seq[Column]]: _*)).thenReturn(df)
+    when(df.where(any[Column])).thenReturn(filteredDF)
+    when(filteredDF.persist(any())).thenReturn(filteredDF)
+    when(filteredDF.schema("part_col").dataType.getClass.getSimpleName).thenReturn("StringType")
+    when(filteredDF.select(any[Column])).thenReturn(filteredDF)
+    when(filteredDF.select(any[Column]).distinct().collect()).thenReturn(collected)
+    when(filteredDF.unpersist()).thenReturn(filteredDF)
 
-  val entity1 = mock[IngestEntity]
-  val entity2 = mock[IngestEntity]
+    when(sqlContext.sql(any[String])).thenReturn(tmpDF)
+    when(tmpDF.select(any[Seq[Column]]: _*)).thenReturn(tmpDF)
+    when(tmpDF.distinct()).thenReturn(distinctDF)
 
-  when(entity1.getDataDatePart).thenReturn("2023-01-01")
-  when(entity2.getDataDatePart).thenReturn("2023-01-02")
+    // Ejecutar la función
+    val resultPath = BoardDataUtil.createTmpTable(
+      sourceFilter = df,
+      target = "target_table",
+      particionesDest = partList,
+      extraFilter = column,
+      sqlContext = sqlContext,
+      path = path
+    )
 
-  when(sqlContext.table("my_table")).thenReturn(dfMock)
-
-  // simulamos que el .where devuelve otro DF
-  when(dfMock.where(any[Column])).thenReturn(filteredDfMock)
-
-  val result = BoardDataUtil.getSourceFilter(
-    partitionType = "data_date_part",
-    source = "my_table",
-    query = List(col("data_date_part")),
-    entities = List(entity1, entity2),
-    sqlContext = sqlContext
-  )
-
-  assert(result eq filteredDfMock)
-}
-
-test("removeDuplicatesFromDataFrame should drop rows with duplicate partitionDateColumn") {
-  val dfMock = mock[DataFrame]
-  val resultDfMock = mock[DataFrame]
-  val withColumnDfMock = mock[DataFrame]
-  val droppedDfMock = mock[DataFrame]
-  val filteredDfMock = mock[DataFrame]
-  val colMock = mock[Column]
-
-  val windowSpec = Window.partitionBy(colMock)
-
-  when(dfMock.withColumn(
-    ArgumentMatchers.eq("maxDataDatePart"),
-    ArgumentMatchers.any[Column]
-  )).thenReturn(withColumnDfMock)
-
-  when(withColumnDfMock.where(colMock === col("maxDataDatePart"))).thenReturn(filteredDfMock)
-  when(filteredDfMock.drop("maxDataDatePart")).thenReturn(droppedDfMock)
-
-  val result = BoardDataUtil.removeDuplicatesFromDataFrame(colMock, dfMock, colMock)
-
-  assert(result eq droppedDfMock)
-}
-
-
-test("repairTable should execute MSCK REPAIR TABLE SQL") {
-  val sqlContext = mock[SQLContext]
-
-  BoardDataUtil.repairTable("target_table", sqlContext)
-
-  verify(sqlContext).sql("set hive.msck.path.validation=ignore")
-  verify(sqlContext).sql("msck repair table target_table")
-}
+    assert(resultPath.contains("tmp_windowsFunction_"))
+  }
