f719332c64c261f65672d0e3074115a8caff5759
Alejandro García Pérez <alegp97@gmail.com>
2025-07-14 13:04:58 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,8 +1,11 @@
-val ds = mock[Dataset[String]]
-when(idLogitDF
-       .map(any[Function1[Row, String]]())   // variante Scala
-       (any())                               // cualquier Encoder[String]
-).thenReturn(ds)
+when(idLogitDF.select(any[Column])).thenReturn(idLogitDF)
+when(idLogitDF.distinct()).thenReturn(idLogitDF)
 
-// 3️⃣  ds.collect()  →  array real
-when(ds.collect()).thenReturn(Array("dummy_id1", "dummy_id2"))
+// 3️⃣  Stub directo hasta collect()
+import org.apache.spark.sql.{Encoder, Encoders, Row}
+when(
+  idLogitDF
+    .map(any[Function1[Row,String]]())          // 1ª lista de parámetros
+    (any[Encoder[String]]())                    // 2ª (implícitos)
+    .collect()
+).thenReturn(Array("dummy_id1","dummy_id2"))
