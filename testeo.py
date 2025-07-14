049ef70e2c3fcc6f5c70427e9b526e12929a2bae
Alejandro García Pérez <alegp97@gmail.com>
2025-07-14 12:38:06 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,5 +1,7 @@
+// 2) Stub de map-scala + collect
 when(
-  idLogitDF.where(any[Column])
-           .map(any[Row ⇒ String])
-           .collect()
+  idLogitDF
+    .map(any[Function1[Row, String]]())          // 1er parámetro
+    (any[Encoder[String]]())                     // 2º parámetro (implícito)
+    .collect()
 ).thenReturn(Array("dummy_id1", "dummy_id2"))
