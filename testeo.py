b4fc3bc680d6c53e93000128480d9d8312045456
Alejandro García Pérez <alegp97@gmail.com>
2025-07-15 14:43:43 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,10 +1,9 @@
 when(
-  idLogitDF.map(
-    any[Function1[Row, String]]()      // la lambda Scala
-  )(                                     // paréntesis del parámetro implícito
-    any[Encoder[String]]()              // el encoder implícito
-  )
-).thenReturn(dsString)
-
-// 3. Stub de collect() sobre ese mismo Dataset[String]
-when(dsString.collect()).thenReturn(Array("dummy_id1", "dummy_id2"))
+  engine
+    .where(anyArg[Column]())
+    // 1ª lista de parámetros
+    .map(anyArg[Function1[Row, _]]())          // r => …   (tipo laxo)
+    // 2ª lista implícita  (Encoder)
+    (Encoders.STRING.asInstanceOf[Encoder[String]])
+    .collect()
+).thenReturn(Array("dummy_id1", "dummy_id2"))
