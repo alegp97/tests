bd8ba0acf9055a961fa13e34907f462bfa06697a
Alejandro García Pérez <alegp97@gmail.com>
2025-07-16 13:37:40 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,69 +1,9 @@
-val sqlCtx  = mock[SQLContext](RETURNS_DEEP_STUBS)
-    val engine  = mock[DataFrame](RETURNS_DEEP_STUBS)
+val engine = mock[DataFrame]
+val engineWhere = mock[DataFrame]
+val engineSelect = mock[DataFrame]
+val engineDistinct = mock[DataFrame]
 
-    // tabla "sae_engine" → engine
-    when(sqlCtx.table("sourcedb.sae_engine")).thenReturn(engine)
-
-    // Stub global hasta collect()
-    when(
-      engine.where(any[Column]())
-            .map(any[Function1[Row,_]]())(any[Encoder[_]]())
-            .collect()
-    ).thenReturn(Array("dummy_id1","dummy_id2"))
-
-    val df = BoardDataUtil.calculateDFModels("sourcedb", K.CREDIT, sqlCtx)
-
-
-
-
-
-
- val sqlCtx  = mock[SQLContext]          // sin deep-stubs
-    val engine  = mock[DataFrame]           // sin deep-stubs
-    val idLogit = mock[DataFrame]           // after where
-    val dsStr   = mock[DataFrame]           // after map (Dataset[String])
-
-    when(sqlCtx.table("sourcedb.sae_engine")).thenReturn(engine)
-
-    // 1. where → idLogit
-    when(engine.where(any[Column]())).thenReturn(idLogit)
-
-    // 2. map → dsStr
-    when(idLogit.map(any[Function1[Row,_]]())(any[Encoder[_]]()))
-      .thenReturn(dsStr)
-
-    // 3. collect sobre dsStr
-    when(dsStr.collect()).thenReturn(Array("dummy_id1","dummy_id2"))
-
-    val df = BoardDataUtil.calculateDFModels("sourcedb", K.CREDIT, sqlCtx)
-
-
-
-
-
-
-
-
-
- private val smartCollect: Answers = (inv: InvocationOnMock) => {
-    if (inv.getMethod.getName == "collect")
-      Array("dummy_id1","dummy_id2")
-    else
-      Answers.RETURNS_DEEP_STUBS.answer(inv)
-  }
-
-
-
-    val sqlCtx = mock[SQLContext](RETURNS_DEEP_STUBS)
-    val engine = mock[DataFrame](
-      withSettings().defaultAnswer(smartCollect)
-    )
-
-    when(sqlCtx.table("sourcedb.sae_engine")).thenReturn(engine)
-
-    val df = BoardDataUtil.calculateDFModels("sourcedb", K.CREDIT, sqlCtx)
-
-
-
-
-
+when(sqlContext.table(s"$sourcedb.sae_engine")).thenReturn(engine)
+when(engine.where(any[Column])).thenReturn(engineWhere)
+when(engineWhere.select(any[Column])).thenReturn(engineSelect)
+when(engineSelect.distinct()).thenReturn(engineDistinct)
