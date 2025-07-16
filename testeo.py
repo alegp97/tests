58ea1e04699b15e65e89ce73585450ce7d69cb00
Alejandro García Pérez <alegp97@gmail.com>
2025-07-16 15:14:23 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,11 +1,45 @@
-val idFixed = mock[Dataset[Row]]
-val mappedDataset = mock[Dataset[String]]
+test("precoalesce con deep mocks") {
+  val sourcedb = "sourcedb"
+  val scope = "some_scope"
+  val filterColumn = mock[Column]
 
-// stub para .map(...)
-when(idFixed.map(
-  mockAny[Function1[Row, String]](),
-  mockAny[Encoder[String]]()
-)).thenReturn(mappedDataset)
+  val sqlContext = mock[SQLContext]
+  val scenarioDF = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
+  val scenarioDataDF = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
+  val scenarioOverrideDF = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
+  val baselineDF = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
+  val timePeriodDF = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
 
-// stub para .collect()
-when(mappedDataset.collect()).thenReturn(Array("dummy_id1", "dummy_id2"))
+  when(sqlContext.table(s"$sourcedb.escenario_vigente")).thenReturn(scenarioDF)
+  when(sqlContext.table(s"$sourcedb.sae_scenario_data")).thenReturn(scenarioDataDF)
+  when(sqlContext.table(s"$sourcedb.sae_scendata_override")).thenReturn(scenarioOverrideDF)
+  when(sqlContext.table(s"$sourcedb.sae_baseline_data")).thenReturn(baselineDF)
+  when(sqlContext.table(s"$sourcedb.sae_time_period")).thenReturn(timePeriodDF)
+
+  when(scenarioDataDF.where(filterColumn)).thenReturn(scenarioDataDF)
+  when(scenarioOverrideDF.where(filterColumn)).thenReturn(scenarioOverrideDF)
+
+  // mocks del map+collect
+  val camposFijos = mock[Dataset[Row]]
+  val camposFijosStr = mock[Dataset[String]]
+  when(camposFijos.map(
+    isA(classOf[Function1[Row, String]]),
+    any[Encoder[String]]
+  )).thenReturn(camposFijosStr)
+  when(camposFijosStr.collect()).thenReturn(Array("dummy_1", "dummy_2"))
+
+  // mock del select final
+  val joinedDF = mock[DataFrame](withSettings().defaultAnswer(RETURNS_DEEP_STUBS))
+  when(joinedDF.select(any[Seq[Column]]: _*)).thenReturn(joinedDF)
+
+  // mock para coalesceByColumns (usamos función externa)
+  val coalesceByColumnsMock = mock[List[Column]]
+  val coalesceByColumnsObj = mock[BoardDataUtilWrapper.type]
+  when(coalesceByColumnsObj.coalesceByColumns(any[DataFrame], any[DataFrame], any[List[String]])).thenReturn(coalesceByColumnsMock)
+
+  // llamada final
+  val result = BoardDataUtil.precoalesce(sqlContext, scope, sourcedb, filterColumn)
+
+  // Assertions opcionales si haces return de algo o invocas lógica importante
+  assert(result != null)
+}
