f56d9d1d33fe30b14e90e6c51e1e44cf8763a4a5
Alejandro García Pérez <alegp97@gmail.com>
2025-07-17 14:26:38 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,21 +1,11 @@
-val row1 = mock[Row]
-when(row1.getAs[BigDecimal]("scenario_type")).thenReturn(BigDecimal(1))
-when(row1.getAs[String]("column_name")).thenReturn("col_abc")
-when(row1.getAs[String]("name")).thenReturn("my_Name")
+  // Simulamos la parte de map(...).toList sobre Dataset
+  import org.apache.spark.sql.RowFactory
+  val nameRow = RowFactory.create("my_Name")
+  when(camposOriginals.collect()).thenReturn(Array(nameRow))
 
-val row2 = mock[Row]
-when(row2.getAs[BigDecimal]("scenario_type")).thenReturn(BigDecimal(2))
-when(row2.getAs[String]("column_name")).thenReturn("col_def")
-when(row2.getAs[String]("name")).thenReturn("my_Name") // mismo nombre para agrupar
-
-val rowGroup = Array(row1, row2)
-
-
-when(camposOriginals.collect()).thenReturn(rowGroup)
-
-
-
-import java.math.BigDecimal as JBigDecimal
-
-when(row1.getAs[JBigDecimal]("scenario_type")).thenReturn(new JBigDecimal("1"))
-when(row2.getAs[JBigDecimal]("scenario_type")).thenReturn(new JBigDecimal("2"))
+  // camposFijos: necesarios si se usan en .map { x => col(...) ... }
+  when(camposFijos.select(any[Seq[Column]])).thenReturn(camposFijos)
+  when(camposFijos.distinct()).thenReturn(camposFijos)
+  val campoFijo1 = RowFactory.create("col_abc")
+  val campoFijo2 = RowFactory.create("col_def")
+  when(camposFijos.collect()).thenReturn(Array(campoFijo1, campoFijo2))
