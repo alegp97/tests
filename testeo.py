fa5ab0371eb371edc86373cb9a4cb282720f98b8
Alejandro García Pérez <alegp97@gmail.com>
2025-07-17 18:02:27 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,39 +1,48 @@
 import org.scalatest.funsuite.AnyFunSuite
-import com.santander.stresstest.config.{HouseKeepingParser, HouseKeepingArgs}
+import com.santander.stresstest.parser.config.{CompactorParser, CompactorArgs}
 
-class HouseKeepingParserTest extends AnyFunSuite {
+class CompactorParserTest extends AnyFunSuite {
 
-  test("should parse required and optional arguments") {
+  test("should parse all arguments correctly") {
     val args = Array(
-      "-d", "my_db",
-      "-t", "my_table",
-      "-m", "4"
+      "-s", "256",
+      "-h", "8",
+      "-i", """{"obj":"value"}""",
+      "-f", "filter_string",
+      "-d", "db_prod",
+      "-t", "tbl_fact"
     )
 
-    val result = HouseKeepingParser.parse(args, HouseKeepingArgs())
-
+    val result = CompactorParser.parse(args, CompactorArgs())
     assert(result.isDefined)
     val config = result.get
-    assert(config.database == "my_db")
-    assert(config.table == "my_table")
-    assert(config.maxPartitions == 4)
+
+    assert(config.sizeFile == 256)
+    assert(config.thread == 8)
+    assert(config.ingestEntity == """{"obj":"value"}""")
+    assert(config.filter == "filter_string")
+    assert(config.database == "db_prod")
+    assert(config.table == "tbl_fact")
   }
 
-  test("should parse only required argument and use defaults for optional") {
-    val args = Array("-d", "default_db")
+  test("should parse only required and use default values") {
+    val args = Array("-d", "only_required_db")
 
-    val result = HouseKeepingParser.parse(args, HouseKeepingArgs())
-
+    val result = CompactorParser.parse(args, CompactorArgs())
     assert(result.isDefined)
     val config = result.get
-    assert(config.database == "default_db")
+
+    assert(config.database == "only_required_db")
+    assert(config.sizeFile == 128)
+    assert(config.thread == 1)
+    assert(config.ingestEntity.isEmpty)
+    assert(config.filter.isEmpty)
     assert(config.table.isEmpty)
-    assert(config.maxPartitions == 1)
   }
 
-  test("should fail if required argument is missing") {
-    val args = Array("-t", "table_only")
-    val result = HouseKeepingParser.parse(args, HouseKeepingArgs())
+  test("should fail when required database is missing") {
+    val args = Array("-t", "missing_db_table")
+    val result = CompactorParser.parse(args, CompactorArgs())
     assert(result.isEmpty)
   }
 }
