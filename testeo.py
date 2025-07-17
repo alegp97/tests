46e3a270737938da898363d563ecc4ab3185e18e
Alejandro García Pérez <alegp97@gmail.com>
2025-07-17 18:05:07 +0200

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,48 +1,78 @@
 import org.scalatest.funsuite.AnyFunSuite
-import com.santander.stresstest.parser.config.{CompactorParser, CompactorArgs}
+import com.santander.stresstest.entity.{BoardsParser, BoardsArgs}
 
-class CompactorParserTest extends AnyFunSuite {
+class BoardsParserTest extends AnyFunSuite {
 
   test("should parse all arguments correctly") {
     val args = Array(
-      "-s", "256",
-      "-h", "8",
-      "-i", """{"obj":"value"}""",
-      "-f", "filter_string",
-      "-d", "db_prod",
-      "-t", "tbl_fact"
+      "-c", "/conf/path/file.conf",
+      "-d", "target_db",
+      "-i", """{"entity":"sample"}""",
+      "-f", "my_filter",
+      "-w", "false",
+      "-r", "credit",
+      "-s", "src_db",
+      "-p", "k1:v1,k2:v2",
+      "-t", "tbl_dest",
+      "-y", "tbl_optional",
+      "-z", "/data/path/",
+      "-o", "dev",
+      "-l", "20250717",
+      "-x", "202507171200",
+      "-u", "tbl_src",
+      "-b", "insert",
+      "-m", "yes"
     )
 
-    val result = CompactorParser.parse(args, CompactorArgs())
+    val result = BoardsParser.parse(args, BoardsArgs())
+
     assert(result.isDefined)
     val config = result.get
-
-    assert(config.sizeFile == 256)
-    assert(config.thread == 8)
-    assert(config.ingestEntity == """{"obj":"value"}""")
-    assert(config.filter == "filter_string")
-    assert(config.database == "db_prod")
-    assert(config.table == "tbl_fact")
+    assert(config.file == "/conf/path/file.conf")
+    assert(config.targetdb == "target_db")
+    assert(config.ingestEntity.contains("entity"))
+    assert(config.filter == "my_filter")
+    assert(config.isWI == false)
+    assert(config.risk == "credit")
+    assert(config.sourcedb == "src_db")
+    assert(config.partitions == Map("k1" -> "v1", "k2" -> "v2"))
+    assert(config.targetTable == "tbl_dest")
+    assert(config.targetTableOptionalName == "tbl_optional")
+    assert(config.path == "/data/path/")
+    assert(config.sandbox == "dev")
+    assert(config.data_date_part == "20250717")
+    assert(config.data_timestamp_part == "202507171200")
+    assert(config.sourceTable == "tbl_src")
+    assert(config.process == "insert")
+    assert(config.is_incremental == "yes")
   }
 
-  test("should parse only required and use default values") {
-    val args = Array("-d", "only_required_db")
+  test("should fail if required arguments are missing") {
+    val args = Array("-c", "only_config.conf") // falta -d y -s
+    val result = BoardsParser.parse(args, BoardsArgs())
+    assert(result.isEmpty)
+  }
 
-    val result = CompactorParser.parse(args, CompactorArgs())
+  test("should assign default values when optional flags are omitted") {
+    val args = Array("-d", "tgt", "-s", "src")
+    val result = BoardsParser.parse(args, BoardsArgs())
     assert(result.isDefined)
     val config = result.get
 
-    assert(config.database == "only_required_db")
-    assert(config.sizeFile == 128)
-    assert(config.thread == 1)
+    assert(config.file.isEmpty)
     assert(config.ingestEntity.isEmpty)
     assert(config.filter.isEmpty)
-    assert(config.table.isEmpty)
-  }
-
-  test("should fail when required database is missing") {
-    val args = Array("-t", "missing_db_table")
-    val result = CompactorParser.parse(args, CompactorArgs())
-    assert(result.isEmpty)
+    assert(config.isWI) // default true
+    assert(config.risk.isEmpty)
+    assert(config.partitions.isEmpty)
+    assert(config.targetTable.isEmpty)
+    assert(config.targetTableOptionalName.isEmpty)
+    assert(config.path.isEmpty)
+    assert(config.sandbox.isEmpty)
+    assert(config.data_date_part.isEmpty)
+    assert(config.data_timestamp_part.isEmpty)
+    assert(config.sourceTable.isEmpty)
+    assert(config.process.isEmpty)
+    assert(config.is_incremental.isEmpty)
   }
 }
